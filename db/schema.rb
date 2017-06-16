0d57f74968e5fda340e1cbdd935cbe404307b307
Ronen Annason <ronenannason83@gmail.com>
2017-06-15 20:10:40 -0400

    ran rails generate migration ChangePartyGuestsToWidgets to generate a new migration, then added rename_table :party_guests, :widgets to ChangePartyToWidgets file, then ran db:migrate, checked schema file, table name changed!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615234617) do
+ActiveRecord::Schema.define(version: 20170616000543) do
 
   create_table "locations", force: :cascade do |t|
     t.string "address"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20170615234617) do
     t.integer "quantity"
   end
 
-  create_table "party_guests", force: :cascade do |t|
+  create_table "widgets", force: :cascade do |t|
     t.string  "first_name"
     t.string  "last_name"
     t.text    "dietary_restrictions"
