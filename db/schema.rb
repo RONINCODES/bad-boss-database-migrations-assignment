6fb4eb0a85acb6cb6398e0cf3e9d65117e6d9f30
Ronen Annason <ronenannason83@gmail.com>
2017-06-15 20:32:36 -0400

    Changed value Type for Column quantity
    ran rails generate migration to change value type in Parts table for column
    quantity.
    Edited migration change file using change_column from integer to decimal.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616000543) do
+ActiveRecord::Schema.define(version: 20170616002141) do
 
   create_table "locations", force: :cascade do |t|
     t.string "address"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20170616000543) do
     t.string  "name"
     t.string  "manufacturer"
     t.decimal "cost"
-    t.integer "quantity"
+    t.decimal "quantity"
   end
 
   create_table "widgets", force: :cascade do |t|
